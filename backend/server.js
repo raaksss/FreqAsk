df4df7a795b3c89e5c63c79db7a290de806a4728
raaksss <rakshitamanocha@gmail.com>
2024-06-27 10:48:59 +0530

    Rectified port issue

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 frontend=process.env.FRONTEND_URL || 'http://localhost:3000';
 console.log(frontend)
 app.use(cors({ origin: frontend }));
