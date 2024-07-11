39ea068ddde50e122d5324467c71c0ad1f753768
raaksss <rakshitamanocha@gmail.com>
2024-07-11 11:51:16 +0530

    Added all the faqs

--- frontend/src/components/Freq/SearchResultComponent.js
+++ frontend/src/components/Freq/SearchResultComponent.js
@@ -1,19 +1,26 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 const SearchResultComponent = ({ results }) => {
+    const navigate=useNavigate();
+
+    const handleClick=()=>{
+        navigate('/freq-ask/search-results');
+    }
   return (
     <div>
       {results.length > 0 ? (
-        results.map((faq) => (
-          <div key={faq._id} className="my-10 p-4 border-b border-gray-300">
+        results.slice(0, 5).map((faq) => (
+          <div key={faq._id} className="my-10 p-4 border-b border-gray-300" onClick={handleClick}>
             <h4 className="text-lg text-left font-semibold">{faq.question}</h4>
             <p className="text-gray-600 text-left">{faq.answer}</p>
           </div>
         ))
       ) : (
-        <p className="text-black-500">
-          {results.length === 0 ? 'No results found for this search.' : ''}
-        </p>
+        results.length === 0 && (
+          <p className="text-black-500">
+            No results found for this search.
+          </p>
+        )
       )}
     </div>
   );
